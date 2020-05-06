e3feff768c1fcd406d9536de78a79769bcc13507
CannaBitss <sata.ckr@gmail.com>
2020-05-07 00:28:19 +0700

    First Commit

--- src/core_io.h
+++ src/core_io.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2017-2019 The VIP developers
+// Copyright (c) 2015-2020 The PIVX developers
+// Copyright (c) 2018-2020 The VIP developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
